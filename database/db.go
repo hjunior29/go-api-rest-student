59538cb16a50f9764b107f1d52f2fc2e8d8d9eb7
hjunior29 <helderjuniorsilvalima@gmail.com>
2023-09-03 11:35:13 -0300

    feat: add db credentials in .env

--- database/db.go
+++ database/db.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"Api-Rest-Gin/models"
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,10 +16,18 @@ var (
 )
 
 func DBConnect() {
-	connection := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	connection := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
 	DB, err = gorm.Open(postgres.Open(connection))
 	if err != nil {
 		log.Panic("Database connection error")
 	}
+
 	DB.AutoMigrate(&models.Student{})
 }
--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"Api-Rest-Gin/database"
 	"Api-Rest-Gin/routes"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+
 	database.DBConnect()
 
 	routes.HandleRequest()
